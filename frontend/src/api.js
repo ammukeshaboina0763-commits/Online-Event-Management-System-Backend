9ead63a291199cbeec2fd26934f06dbfdd0617b5
Deekshitha <ammukeshaboina0763@gmail.com>
2026-09-25 16:52:27 +0530

    Update backend API URL

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://localhost:5000/api/events'
+    baseURL:'https://online-event-management-system-back.vercel.app/api/events'
 });
 
 export default api;
